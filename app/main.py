a2c2d9691e1fbfde2259824555d53b8e4071f1f6
Jeison Araya Mena <57369279+jeison-araya@users.noreply.github.com>
2023-06-01 23:33:41 -0600

    Show grafic

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
+import matplotlib.pyplot as plt
 from app.schemas import Interval
 from app.natural_cubic_trazer import NaturalCubicTracer
-
+import numpy as np
 
 S_1 = Interval(x=[1, 2, 5, 6, 7, 8, 10, 13, 17],
                y=[3.0, 3.7, 3.9, 4.2, 5.7, 6.6, 7.1, 6.7, 4.5])
@@ -11,4 +12,29 @@ S_2 = Interval(x=[17, 20, 23, 24, 25, 27, 27.7],
 S_3 = Interval(x=[27.7, 28, 29, 30],
                y=[4.1, 4.3, 4.1, 3.0])
 
-NaturalCubicTracer(intervals=[S_1, S_2, S_3]).show_info()
+S = NaturalCubicTracer(intervals=[S_1, S_2, S_3])
+
+S.show_info()
+
+
+x = []
+y = []
+
+for i in np.arange(1.0, 30.0, 1):
+    x.append(i)
+    y.append(S.aproximate(x=i))
+
+# Set the x-axis and y-axis values
+plt.plot(x, y)
+
+# Set the minimum and maximum values for x-axis and y-axis
+plt.xlim(0, 30)
+plt.ylim(0, 8)
+
+# Add labels and title
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Plot of Points')
+
+# Display the plot
+plt.show()
--- app/natural_cubic_trazer.py
+++ app/natural_cubic_trazer.py
@@ -8,7 +8,9 @@ class NaturalCubicTracer:
     n = None
     h = []
     a = []
+    b = []
     c = []
+    d = []
 
     def __init__(self, intervals: List[Interval]):
         self.intervals = intervals
@@ -16,6 +18,8 @@ class NaturalCubicTracer:
         self.h = [interval.get_height() for interval in intervals]
         self.a = self._calculate_a_values()
         self.c = self._calculate_c_values()
+        self.b = self._calculate_b_values()
+        self.d = self._calculate_d_values()
 
     def _calculate_a_values(self):
         a = []
@@ -43,6 +47,7 @@ class NaturalCubicTracer:
                 c=c,
                 i=i)
             c_i += f'c_{i} '
+            print(equation)
             equations.append(Eq(equation['equation'], equation['solution']))
 
         # Remove last space
@@ -59,12 +64,36 @@ class NaturalCubicTracer:
 
     def _build_c_i_equation(self, h: List[float], a: List[float], c: List[float], i: int) -> dict:
         return {
-            'equation': (h[i-1] * c[i-1]) + (2.0 * (h[i-1] + h[i])) * c[i] + (h[i]*c[i+1]),
-            'solution': (3.0 / h[i]) * (a[i+1] - a[i]) - (3.0 / h[i-1]) * (a[i] - a[i-1])
+            'equation': (h[i-1] * c[i-1]) + ((2.0 * (h[i-1] + h[i])) * c[i]) + (h[i]*c[i+1]),
+            'solution': ((3.0 / h[i]) * (a[i+1] - a[i])) - ((3.0 / h[i-1]) * (a[i] - a[i-1]))
         }
+
+    def _calculate_b_values(self):
+        b = []
+        for i in range(self.n):
+            b.append((1.0 / self.h[i]) * (self.a[i+1] - self.a[i]) -
+                     (self.h[i] / 3.0) * (2.0 * self.c[i] + self.c[i+1]))
+
+        return b
+
+    def _calculate_d_values(self):
+        d = []
+        for i in range(self.n):
+            d.append((self.c[i+1] - self.c[i]) / (3.0 * self.h[i]))
+
+        return d
+
+    def aproximate(self, x: float):
+        for i in range(self.n):
+            if self.intervals[i].x[0] <= x <= self.intervals[i].x[-1]:
+                return self.a[i] + self.b[i] * (x - self.intervals[i].x[0]) + self.c[i] * (x - self.intervals[i].x[0]) ** 2 + self.d[i] * (x - self.intervals[i].x[0]) ** 3
+
+
 
     def show_info(self):
         print(f'n: {self.n}')
         print(f'h_i: {self.h}')
         print(f'a_i: {self.a}')
+        print(f'b_i: {self.b}')
         print(f'c_i: {self.c}')
+        print(f'd_i: {self.d}')
