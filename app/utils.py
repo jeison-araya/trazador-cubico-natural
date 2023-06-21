7f0c4be09b641f00cf16aef2ecf1c749baf14ee6
Jeison Araya Mena <57369279+jeison-araya@users.noreply.github.com>
2023-06-21 17:07:52 -0600

    Limit of graphic removed

--- app/utils.py
+++ app/utils.py
@@ -4,9 +4,6 @@ import matplotlib.pyplot as plt
 def show_graphic(x, y):
     plt.plot(x, y)
 
-    plt.xlim(0, 30)
-    plt.ylim(0, 8)
-
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.title('Plot of Points')
