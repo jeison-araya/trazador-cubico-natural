ccaca68fcf22f5be3c9801d2a0da1dc8665f882b
Jeison Araya Mena <57369279+jeison-araya@users.noreply.github.com>
2023-06-21 20:21:12 -0600

    Old files removed

--- app/main.py
+++ app/main.py
@@ -1,42 +1,52 @@
-import numpy as np
-from app.natural_cubic_spline import Interval, NaturalCubicSpline
-from app.utils import show_graphic
-
-# x = [-0.5, -0.25, 0]
-# y = [-0.025, 0.335, 1.1]
-# interval = Interval(x=x, y=y)
-# print(interval)
-# y_prima = interval.aproximate(x=-0.3)
-# print(y_prima)
-
-# S_1 = Interval(x=[1, 2, 5, 6, 7, 8, 9, 10, 13, 17],
-#                y=[3.0, 3.7, 3.9, 4.2, 5.7, 6.6, 6.9, 7.1, 6.7, 4.5])
-
-# S_2 = Interval(x=[17, 20, 23, 24, 25, 27, 27.7],
-#                y=[4.5, 7.0, 6.1, 5.6, 5.8, 5.2, 4.1])
-
-# S_3 = Interval(x=[27.7, 28, 29, 30],
-#                y=[4.1, 4.3, 4.1, 3.0])
-
-# S = NaturalCubicSpline(intervals=[S_1, S_2, S_3])
-
-# print(S.aproximate(x=26))
+from app.natural_cubic_spline import Interval
+from app.natural_cubic_spline import NaturalCubicSpline
 
 
+def run_example_1():
+    x = [0.1, 0.2, 0.3, 0.4]
+    y = [-0.62049958, -0.28398668, 0.00660095, 0.24842440]
+    S_1 = Interval(x=x, y=y)
 
-x = [0.1, 0.2, 0.3, 0.4]
-y = [-0.62049958, -0.28398668, 0.00660095, 0.24842440]
-interval = Interval(x=x, y=y)
+    S = NaturalCubicSpline(intervals=[S_1])
+
+    x = 0.25
+    print(f'S({x}) = { S.aproximate(x=x)}')
+
+    x = 0.35
+    print(f'S({x}) = { S.aproximate(x=x)}')
+
+    S.draw_graphic(start=0.1, end=0.4, step=0.01, filename='images/test_1.png')
 
 
-print(interval)
+def run_example_2():
+    S_1 = Interval(x=[1, 2, 5, 6, 7, 8, 9, 10, 13, 17],
+                   y=[3.0, 3.7, 3.9, 4.2, 5.7, 6.6, 6.9, 7.1, 6.7, 4.5])
 
-x = []
-y = []
+    S_2 = Interval(x=[17, 20, 23, 24, 25, 27, 27.7],
+                   y=[4.5, 7.0, 6.1, 5.6, 5.8, 5.2, 4.1])
 
-for i in np.arange(0.1, 0.4, 0.01):
-    x.append(i)
-    y.append(interval.aproximate(x=i))
-    print(f"{i} -> {interval.aproximate(x=i)}")
+    S_3 = Interval(x=[27.7, 28, 29, 30],
+                   y=[4.1, 4.3, 4.1, 3.0])
 
-show_graphic(x=x, y=y)
+    S = NaturalCubicSpline(intervals=[S_1, S_2, S_3])
+
+    x = 26
+    print(f'S({x}) = { S.aproximate(x=x)}')
+
+    S.draw_graphic(start=1.0, end=30.0, step=0.1, filename='images/test_2.png')
+
+
+def run_example_3():
+    x = [-0.5, -0.25, 0]
+    y = [-0.025, 0.335, 1.1]
+
+    S_1 = Interval(x=x, y=y)
+
+    S = NaturalCubicSpline(intervals=[S_1])
+
+    S.draw_graphic(start=-0.5, end=0, step=0.01, filename='images/test_3.png')
+
+
+run_example_1()
+run_example_2()
+run_example_3()
--- app/natural_cubic_spline.py
+++ app/natural_cubic_spline.py
@@ -1,122 +1,96 @@
+"""
+Using the algorithm from the book "Numerical Analysis" by Burden and Faires
+"""
+from sympy import symbols
 from typing import List
-from sympy import symbols, solve, Eq
-
-
+import matplotlib.pyplot as plt
+import numpy as np
 class Interval:
-    x: List[float] = []
-    y: List[float] = []
-    h: List[float] = []
-    n: int = None
-    a = []
-    b = []
-    c = []
-    d = []
-    s = []
-
     def __init__(self, x, y):
         self.x = x
-        self.y = y
-        self.n = len(x) - 1
-        self.h = self._calculate_heights()
-        self.a = self._calculate_a_values()
-        self.c = self._calculate_c_values()
-        self.b = self._calculate_b_values()
-        self.d = self._calculate_d_values()
-        self.s = self._build_s()
+        self.a = y
+        self.n = len(x)
+        self.h: List[float] = [0.0] * (self.n - 1)
+        self.alpha: List[float] = [0.0] * (self.n - 1)
+        self.l: List[float] = [0.0] * self.n
+        self.u: List[float] = [0.0] * (self.n - 1)
+        self.z: List[float] = [0.0] * self.n
+        self.b: List[float] = [0.0] * (self.n - 1)
+        self.c: List[float] = [0.0] * (self.n)
+        self.d: List[float] = [0.0] * (self.n - 1)
+        self.s = self.build_s()
 
-    def _calculate_a_values(self):
-        return self.y
+    def run_algorithm(self):
+        self.step_1()
+        self.step_2()
+        self.step_3()
+        self.step_4()
+        self.step_5()
+        self.step_6()
 
-    def _calculate_c_values(self):
-        c = [0]  # c_0 = 0
+    def step_1(self):
+        for i in range(0, self.n - 1):
+            self.h[i] = self.x[i+1] - self.x[i]
 
-        for j in range(1, self.n):
-            c.append(symbols(f'c_{j}'))
+    def step_2(self):
+        for i in range(1, self.n - 1):
+            self.alpha[i] = (3/self.h[i])*(self.a[i+1] - self.a[i]) - \
+                (3/self.h[i-1])*(self.a[i] - self.a[i-1])
 
-        c.append(0)  # c_n = 0
-        c_j = ''
-        equations = []
+    def step_3(self):
+        self.l[0] = 1
+        self.u[0] = 0
+        self.z[0] = 0
 
-        for j in range(1, self.n):
-            equation = self._build_c_j_equation(
-                h=self.h,
-                a=self.a,
-                c=c,
-                j=j)
-            c_j += f'c_{j} '
+    def step_4(self):
+        for i in range(1, self.n - 1):
+            self.l[i] = 2*(self.x[i+1] - self.x[i-1]) - self.h[i-1]*self.u[i-1]
+            self.u[i] = self.h[i]/self.l[i]
+            self.z[i] = (self.alpha[i] - self.h[i-1]*self.z[i-1])/self.l[i]
 
-            equations.append(Eq(equation['equation'], equation['solution']))
+    def step_5(self):
+        self.l[self.n - 1] = 1
+        self.z[self.n - 1] = 0
+        self.c[self.n - 1] = 0
 
-        c_j = c_j[:-1]  # Remove last space
+    def step_6(self):
+        for j in range(self.n - 2, -1, -1):
+            self.c[j] = self.z[j] - self.u[j]*self.c[j+1]
+            self.b[j] = (self.a[j+1] - self.a[j])/self.h[j] - \
+                self.h[j]*(self.c[j+1] + 2*self.c[j])/3
+            self.d[j] = (self.c[j+1] - self.c[j])/(3*self.h[j])
 
-        solutions: dict = solve((equations), (symbols(c_j)))
-
-        # Replace c_j with the solution
-        for j in range(1, self.n):
-            solution = solutions.popitem()
-            c[j] = solution[1]
-
-        return c
-
-    def _build_c_j_equation(self, h: List[float], a: List[float], c: List[float], j: int) -> dict:
-
-        return {
-            'equation': (h[j-1] * c[j-1]) + ((2.0 * (h[j-1] + h[j])) * c[j]) + (h[j]*c[j+1]),
-            'solution': ((3.0 / h[j]) * (a[j+1] - a[j])) - ((3.0 / h[j-1]) * (a[j] - a[j-1]))
-        }
-
-    def _calculate_b_values(self):
-        b = []
-
-        for j in range(self.n):
-            b.append((1.0 / self.h[j]) * (self.a[j+1] - self.a[j]) -
-                     (self.h[j] / 3.0) * (2.0 * self.c[j] + self.c[j+1]))
-        return b
-
-    def _calculate_d_values(self):
-        d = []
-
-        for j in range(self.n):
-            d.append((self.c[j+1] - self.c[j]) / (3.0 * self.h[j]))
-        return d
-
-    def _calculate_heights(self):
-        heights = []
-
-        for i in range(len(self.x) - 1):
-            heights.append(self.x[i+1] - self.x[i])
-
-        return heights
-
-    def _build_s(self):
-        s = []
-
-        for j in range(self.n):
-            s.append(self._build_s_j(j))
-
-        return s
-
-    def _build_s_j(self, j: int, x=symbols('x')):
-        return self.a[j] + (self.b[j] * (x - self.x[j])) + (self.c[j] * (x - self.x[j])**2) + (self.d[j] * (x - self.x[j])**3)
+    def build_s(self):
+        self.run_algorithm()
+        x = symbols('x')
+        return [self.a[j] + self.b[j]*(x - self.x[j]) + self.c[j]*(x - self.x[j])**2 + self.d[j]*(x - self.x[j])**3 for j in range(0, self.n - 1)]
 
     def aproximate(self, x: float):
         """
         Returns aproximation of f(x) using the interval
         """
-        for j in range(self.n):
+        for j in range(self.n - 1):
             if self.x[j] <= x <= self.x[j+1]:
                 return self.s[j].subs('x', x)
 
     def __str__(self) -> str:
         return f"""
-        x: {self.x}
-        y: {self.y}
+
+        Interval: {self.x}
         h: {self.h}
+        --------------------
+        alpha: {self.alpha}
+        l: {self.l}
+        u: {self.u}
+        z: {self.z}
+        --------------------
         a: {self.a}
         b: {self.b}
         c: {self.c}
         d: {self.d}
-        s: {[f'S_{j}: {s}' for j, s in enumerate(self.s)]}
+        --------------------
+        s:
+        {[f'S_{j}: {s}' for j, s in enumerate(self.s)]}
         """
 
 
@@ -133,3 +107,16 @@ class NaturalCubicSpline:
         for j, interval in enumerate(self.intervals):
             if interval.x[0] <= x <= interval.x[-1]:
                 return interval.aproximate(x)
+
+    def __str__(self) -> str:
+        return '\n'.join([str(interval) for interval in self.intervals])
+
+    def draw_graphic(self, start: float, end: float, step: float, filename: str):
+        x = []
+        y = []
+        plt.figure()
+        for i in np.arange(start, end, step):
+            x.append(i)
+            y.append(self.aproximate(x=i))
+        plt.plot(x, y)
+        plt.savefig(filename)
