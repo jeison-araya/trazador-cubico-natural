28c5fc026eed93bfb41004d996d984cebfaf7d53
Jeison Araya Mena <57369279+jeison-araya@users.noreply.github.com>
2023-06-21 20:02:53 -0600

    Upgrade done

--- app/main.py
+++ app/main.py
@@ -9,26 +9,34 @@ from app.utils import show_graphic
 # y_prima = interval.aproximate(x=-0.3)
 # print(y_prima)
 
-S_1 = Interval(x=[1, 2, 5, 6, 7, 8, 9, 10, 13, 17],
-               y=[3.0, 3.7, 3.9, 4.2, 5.7, 6.6, 6.9, 7.1, 6.7, 4.5])
+# S_1 = Interval(x=[1, 2, 5, 6, 7, 8, 9, 10, 13, 17],
+#                y=[3.0, 3.7, 3.9, 4.2, 5.7, 6.6, 6.9, 7.1, 6.7, 4.5])
 
-S_2 = Interval(x=[17, 20, 23, 24, 25, 27, 27.7],
-               y=[4.5, 7.0, 6.1, 5.6, 5.8, 5.2, 4.1])
+# S_2 = Interval(x=[17, 20, 23, 24, 25, 27, 27.7],
+#                y=[4.5, 7.0, 6.1, 5.6, 5.8, 5.2, 4.1])
 
-S_3 = Interval(x=[27.7, 28, 29, 30],
-               y=[4.1, 4.3, 4.1, 3.0])
+# S_3 = Interval(x=[27.7, 28, 29, 30],
+#                y=[4.1, 4.3, 4.1, 3.0])
 
-S = NaturalCubicSpline(intervals=[S_1, S_2, S_3])
+# S = NaturalCubicSpline(intervals=[S_1, S_2, S_3])
 
-print(S.aproximate(x=26))
+# print(S.aproximate(x=26))
 
+
+
+x = [0.1, 0.2, 0.3, 0.4]
+y = [-0.62049958, -0.28398668, 0.00660095, 0.24842440]
+interval = Interval(x=x, y=y)
+
+
+print(interval)
 
 x = []
 y = []
 
-for i in np.arange(1.0, 30.0, 0.5):
+for i in np.arange(0.1, 0.4, 0.01):
     x.append(i)
-    y.append(S.aproximate(x=i))
-    print(f"{i} -> {S.aproximate(x=i)}")
+    y.append(interval.aproximate(x=i))
+    print(f"{i} -> {interval.aproximate(x=i)}")
 
 show_graphic(x=x, y=y)
---
+++ app/main_2.py
@@ -0,0 +1,52 @@
+import numpy as np
+from app.natural_cubic_spline_2 import Interval
+from app.natural_cubic_spline_2 import NaturalCubicSpline
+from app.utils import show_graphic
+
+# x = [-0.5, -0.25, 0]
+# y = [-0.025, 0.335, 1.1]
+# interval = Interval(x=x, y=y)
+# print(interval)
+# y_prima = interval.aproximate(x=-0.3)
+# print(y_prima)
+
+S_1 = Interval(x=[1, 2, 5, 6, 7, 8, 9, 10, 13, 17],
+               y=[3.0, 3.7, 3.9, 4.2, 5.7, 6.6, 6.9, 7.1, 6.7, 4.5])
+
+S_2 = Interval(x=[17, 20, 23, 24, 25, 27, 27.7],
+               y=[4.5, 7.0, 6.1, 5.6, 5.8, 5.2, 4.1])
+
+S_3 = Interval(x=[27.7, 28, 29, 30],
+               y=[4.1, 4.3, 4.1, 3.0])
+
+S = NaturalCubicSpline(intervals=[S_1, S_2, S_3])
+
+# print(S)
+
+x = []
+y = []
+
+for i in np.arange(1.0, 30.0, 0.1):
+    x.append(i)
+    y.append(S.aproximate(x=i))
+    # print(f"{i} -> {S.aproximate(x=i)}")
+
+show_graphic(x=x, y=y)
+
+
+# x = [0.1, 0.2, 0.3, 0.4]
+# y = [-0.62049958, -0.28398668, 0.00660095, 0.24842440]
+# interval = Interval(x=x, y=y)
+
+# print('\n\nn')
+# print(interval)
+
+# x = []
+# y = []
+
+# for i in np.arange(0.1, 0.4, 0.01):
+#     x.append(i)
+#     y.append(interval.aproximate(x=i))
+#     print(f"{i} -> {interval.aproximate(x=i)}")
+
+# show_graphic(x=x, y=y)
